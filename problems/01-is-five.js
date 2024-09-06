37817d35ab00f91b4f9afd97c528bc4fc969040c
rmdelapaz <raydelapaz@gmail.com>
2024-09-05 17:19:31 -0700

    Changes to be committed: modified:   problems/01-is-five.js

--- problems/01-is-five.js
+++ problems/01-is-five.js
@@ -4,7 +4,9 @@ a string of 'is five' if the number is equal to 5 and `null` if it is not.
 */
 
 // Your code here 
+function isFive(number){
 
+}
 // console.log(isFive(5));      // => 'is five'
 // console.log(isFive(13));     // => null
 // console.log(isFive("apple")) // => null
